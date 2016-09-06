7df910905d96aa7497c7876fd29312feeef4fb15
Md.Shamim Miah <shamim.ict0754@gmail.com>
2016-09-06 16:22:35 +0600

    Initial commit

---
+++ src/main/java/com/javaaround/dpattern/App.java
@@ -0,0 +1,13 @@
+package com.javaaround.dpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
